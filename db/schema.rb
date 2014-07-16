5d377eb32b188e8e3f1480f8efd4ad640271402d
ocowchun <ocowchun@gmail.com>
2014-07-16 11:37:53 +0800

    CreateOrderItems

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716033707) do
+ActiveRecord::Schema.define(version: 20140716033744) do
 
   create_table "members", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "order_items", force: true do |t|
+    t.integer  "member_id"
+    t.integer  "order_id"
+    t.integer  "amount"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

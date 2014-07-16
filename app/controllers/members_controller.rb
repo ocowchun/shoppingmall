9709bce7b7ed0da7ff7bc2a2020642bd24f77363
ocowchun <ocowchun@gmail.com>
2014-07-16 11:46:38 +0800

    New: add create member

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,26 @@
 class MembersController < ApplicationController
+
+  def index
+    @members=Member.all
+  end
+
+  def new
+    @member=Member.new
+  end
+
+  def create
+    @member=Member.new(member_params)
+
+    if @member.save
+      redirect_to members_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:first_name,:last_name)
+  end
 end
